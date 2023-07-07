a24c691d8cdd4d3229a556e3a0022da6ea9bf3e5
ramazanorcun <ramazanorcun07@gmail.com>
2023-07-07 13:27:22 +0300

    second commit

--- frontend/src/Services/ProductService.js
+++ frontend/src/Services/ProductService.js
@@ -20,15 +20,3 @@ export const addProductService = async (data) => {
     throw new Error("Error fetching categories");
   }
 };
-export const deleteproduct = async (data) => {
-  try {
-    const response = await axios
-      .post("http://127.0.0.1:8000/api/deleteProduct", data)
-      .then((response) => {
-        console.log(response.data);
-      });
-    return response.data;
-  } catch (error) {
-    throw new Error("Error fetching categories");
-  }
-};
